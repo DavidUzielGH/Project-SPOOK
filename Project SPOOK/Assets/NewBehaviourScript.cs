72ca826ec074fe32443133f009108f1846d37ae6
DavidUzielGH <daviduziel.gh@gmail.com>
2018-07-28 12:01:02 -0500

    Testing

---
+++ Project SPOOK/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+    bool doesntmatter = false;
+    public SpriteRenderer spritePlayer;
+	void Start () {
+        spritePlayer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (doesntmatter == false)
+        {
+            spritePlayer.transform.SetPositionAndRotation(new Vector2(3, 3), spritePlayer.transform.rotation);
+            doesntmatter = true;
+        }
+        else if (doesntmatter == true)
+        {
+            spritePlayer.transform.SetPositionAndRotation(new Vector2(2, 2), spritePlayer.transform.rotation);
+            doesntmatter = false;
+        }
+	}
+}
