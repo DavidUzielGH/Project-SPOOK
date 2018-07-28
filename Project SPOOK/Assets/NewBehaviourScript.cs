8dd54e09ec9e40f1190d81c63a9d07e1f2b8aea6
DavidUzielGH <daviduziel.gh@gmail.com>
2018-07-28 13:29:25 -0500

    yup

--- Project SPOOK/Assets/NewBehaviourScript.cs
+++ Project SPOOK/Assets/NewBehaviourScript.cs
@@ -6,21 +6,20 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
     bool doesntmatter = false;
-    public SpriteRenderer spritePlayer;
 	void Start () {
-        spritePlayer = GetComponent<SpriteRenderer>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (doesntmatter == false)
         {
-            spritePlayer.transform.SetPositionAndRotation(new Vector2(3, 3), spritePlayer.transform.rotation);
+            transform.SetPositionAndRotation(new Vector2(3, 3), transform.rotation);
             doesntmatter = true;
         }
         else if (doesntmatter == true)
         {
-            spritePlayer.transform.SetPositionAndRotation(new Vector2(2, 2), spritePlayer.transform.rotation);
+            transform.SetPositionAndRotation(new Vector2(2, 2), transform.rotation);
             doesntmatter = false;
         }
 	}
