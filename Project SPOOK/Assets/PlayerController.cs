b5321a79f964c7f8a05b8bda4203f48c7cb67d87
DavidUzielGH <daviduziel.gh@gmail.com>
2018-08-02 13:58:25 -0500

    Cambio al controlador
    +Cambio del Animator
    +Cambio del Script PlayerController

--- Project SPOOK/Assets/PlayerController.cs
+++ Project SPOOK/Assets/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour { 
     Animator movementAnimator;
     public int moveSpeed;
+    int lastIdle;
     void Start()
     {
         movementAnimator = GetComponent<Animator>();
@@ -13,22 +14,31 @@ public class PlayerController : MonoBehaviour {
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.position += Vector3.down * +moveSpeed * Time.deltaTime;
-            movementAnimator.SetInteger("Animation State", 0);
+            setAnimatorParameters(0);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * +moveSpeed * Time.deltaTime;
-            movementAnimator.SetInteger("Animation State", 1);
+            setAnimatorParameters(1);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * +moveSpeed * Time.deltaTime;
-            movementAnimator.SetInteger("Animation State", 3);
+            setAnimatorParameters(3);
         }
         else if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += Vector3.up * +moveSpeed * Time.deltaTime;
-            movementAnimator.SetInteger("Animation State", 2);
+            setAnimatorParameters(2);
         }
+        if (!Input.anyKey)
+        {
+            movementAnimator.SetBool("isIdle", true);
+        }
+    }
+    private void setAnimatorParameters(int value)
+    {
+        movementAnimator.SetInteger("Animation State", value);
+        movementAnimator.SetBool("isIdle", false);
     }
 }
